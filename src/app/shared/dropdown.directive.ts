29fc4aa7f99f8a734e64d57eb1c986f6bd15395c
vanpeta <carlosallendesanz@gmail.com>
2017-04-17 16:25:15 -0700

    finished lecture 93

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,16 @@
+import { Directive, HostListener, HostBinding } from '@angular/core';
+
+@Directive({
+	selector: '[appDropdown]'
+})
+export class DropdownDirective {
+	@HostBinding('class.open') isOpen: boolean = false;
+
+	constructor() {
+
+	}
+
+	@HostListener('click') toggleOpen(eventData: Event) {
+		this.isOpen = !this.isOpen;
+	}
+}
